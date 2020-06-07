7c096df16574060a4c039986cc1308a08ed91124
aleciobr <50933242+aleciobr@users.noreply.github.com>
2020-06-07 03:51:04 -0300

    ultima_att_aula2

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -20,7 +20,7 @@ function getCities(event) {
     const ufValue = event.target.value
 
     const indexOfSelectedState = event.target.selectedIndex
-    stateInput.value= event.target.options[indexOfSelectedState].text
+    stateInput.value = event.target.options[indexOfSelectedState].text
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
