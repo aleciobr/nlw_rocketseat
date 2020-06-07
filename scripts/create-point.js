ce62c417daad8cd4a946a7ac450d84388987a928
aleciobr <50933242+aleciobr@users.noreply.github.com>
2020-06-07 03:31:26 -0300

    Update create-point.js

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -14,15 +14,16 @@ function populateUFs(){
 populateUFs()
 
 function getCities(event) {
-    const citySelect = document.querySelector("select[name=city]")
-    //const stateInput = document.querySelector("input=[name=state]")
+    const citySelect = document.querySelector("[name=city]")
+  //  const stateInput = document.querySelector("[name=state]")
 
     const ufValue = event.target.value
 
-  //  const indexOfSelectedState = EventTarget.selectedIndex
-   // stateInput.value= event.target.options[indexOfSelectedState]
+    //const indexOfSelectedState = event.target.selectedIndex
+   // stateInput.value= event.target.options[indexOfSelectedState].text
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
 
     fetch(url)
     .then(res=> res.json())
@@ -40,7 +41,7 @@ function getCities(event) {
 
 document
     .querySelector("select[name=uf]")
-    .addEventListener("change", getCities)
+    .addEventListener("change", getCities())
 
 
     
